0b08823764c1bea744803c8d33fe198c1a988fde
Kushagra Saxena <128407478+kushagra1934@users.noreply.github.com>
2024-09-04 18:23:42 +0000

    updated lecture 5

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello") //we are using nodejs and not browser so it gives error here
+
+console.log(3
+    +
+    3
+) //code readibility should be high 
+
+console.log("Kushagra")
+
+let name ="kushagra"
+let age=18
+let isLoggedIn=false
+
+
+//number => 2 to power 53
+//bigint
+//string =>""
+// boolean =>true/false
+//null => standalone value
+//undefined =>
+//symbol=> unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
