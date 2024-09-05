1210d3270f5c5acfab8e99946be01dda42bca2b4
Kushagra Saxena <128407478+kushagra1934@users.noreply.github.com>
2024-09-05 14:03:01 +0000

    conversion of datatypes lec 6

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,46 @@
+// let score ="33ba"
+
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+// string
+// number
+// NaN
+
+// let hello= null
+// console.log(typeof hello);
+// let valueInNumber1=Number(hello)
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);
+// object
+// number
+// 0
+
+
+// let hello= undefined
+// console.log(typeof hello);
+// let valueInNumber1=Number(hello)
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);
+// undefined
+// number
+// NaN
+
+let hello= true
+console.log(typeof hello);
+let valueInNumber1=Number(hello)
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+boolean
+number
+1
+
+// "33" => 33
+// "33abc" => NaN
+// true=> 1; false=>0
+
+
+//similarly for boolean and string
+
