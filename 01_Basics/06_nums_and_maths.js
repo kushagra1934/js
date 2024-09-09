a93ae6b6f468f62cb8403862581f7d63159691f1
Kushagra Saxena <128407478+kushagra1934@users.noreply.github.com>
2024-09-09 06:48:02 +0000

    lecture 12

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,45 @@
+const score=400
+console.log(score);
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber=23.5775
+console.log(otherNumber.toPrecision(3));
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-In'));
+
+
+//++++++++++++++++MATHS+++++++++++++++++++++++++++++
+
+
+//by default comes with js
+
+// console.log(Math);
+//has many functions and many functions of maths 
+
+
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+
+//rewatch this 
+
+console.log(Math.random());
+console.log(Math.random()*10+1 );
+
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min)
+
+
+
+
+
