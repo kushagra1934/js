8408b37a92529d9309aa528b4ca7d90b059cce48
Kushagra Saxena <128407478+kushagra1934@users.noreply.github.com>
2024-09-22 13:37:56 +0000

    lecture 26

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,37 @@
+//basic if else loop tutorial
+
+const balance=1000
+// if ( balance>500) console.log("test"); //IMPLICIT SCOPE
+// this type of practice is not good
+
+//basic nested loops
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//falsy values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+
+// false==0 (true)
+
+//NUllish Coalescing Operator (??): null undefined
+
+let val1;
+// val1=5 ?? 10
+//val1= null ?? 10
+
+console.log(val1);
+
+//ternary operator
+
+// condition?true:false;
